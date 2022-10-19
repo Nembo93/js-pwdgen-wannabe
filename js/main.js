8972952e2b7e99d306c3a2951527822a01b2ebd0
nembo93 <francesco.c193@libero.it>
2022-10-19 14:43:20 +0200

    Aggiunge funzionalità JS per generare restituire in output una password tramite immissionione di dati in input

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Chiedi all'utente il suo nome
+let firstName = prompt (`Inserisci il tuo nome`);
+console.log(typeof firstName);
+
+// Chiedi all'utente il suo cognome
+let lastName = prompt (`Inserisci il tuo cognome`);
+console.log(typeof lastName);
+
+// Chiedi all'utente il suo colore preferito
+let favColor = prompt (`Inserisci il tuo colore preferito`);
+console.log(typeof favColor);
+
+// Genera la password concatenando i valori delle variabili dichiarate
+let user_pdw = firstName + lastName + favColor;
+console.log(user_pdw);
+
+// Visualizza la password in HTML
+// document.getElementById(`user_pdw`);innerHTML = user_pdw;
+document.getElementById(`user_pdw`).innerHTML = user_pdw;
